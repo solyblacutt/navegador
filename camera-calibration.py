2569f69b6894d2ff8e97e6e3960be777871e51bf
Soly Blacutt <solange.blacutt@ing.austral.edu.ar>
2024-08-12 23:40:47 -0300

    finalizado tutorial camibracion camera

--- camera-calibration.py
+++ camera-calibration.py
@@ -20,12 +20,15 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
  
     # Find the chess board corners
+    # 7x6 por el grid de la foto, el patron que tiene que encontrar
+    # ret == True si encuentra el patron
+    # puedo usar un patron circular, ventaja: requiere menos imagenes
     ret, corners = cv.findChessboardCorners(gray, (7,6), None)
  
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
- 
+        # mejoro la precision con cornerSubPix()
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
  
@@ -34,4 +37,38 @@ for fname in images:
         cv.imshow('img', img)
         cv.waitKey(500)
  
+# cchequear posicion de linea
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# refino la matriz
+# left12 es una imagen nueva, no del set que paso
+img = cv.imread('left12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# --------- 2 formas de undistorsionar ------------
+
+# 1)
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+ 
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
+
+# 2)
+# undistort
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+ 
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst) #guardo la img con el roi de interes
+
+# falta guardar la matriz, 'newcameramtx' ?
+
+# es necesario calcular el error? shall we?
+
 cv.destroyAllWindows()
