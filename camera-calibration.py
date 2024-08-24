71f52ff9214d3dc635ea6934327e7f789208a0e5
Solange Blacutt <72108477+solyblacutt@users.noreply.github.com>
2024-08-24 18:03:32 +0000

    creación de un env. corre todo bien

--- camera-calibration.py
+++ camera-calibration.py
@@ -1,31 +1,31 @@
 import numpy as np
 import cv2 as cv
 import glob
+from matplotlib import pyplot as plt
  
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
  
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
  
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
  
-images = glob.glob('/workspaces/navegador/Calibracion/*.png')
+images = glob.glob('calibration/*.png')
 
 for fname in images:
     
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
- 
     # Find the chess board corners
     # 7x6 por el grid de la foto, el patron que tiene que encontrar
     # ret == True si encuentra el patron
     # puedo usar un patron circular, ventaja: requiere menos imagenes
-    ret, corners = cv.findChessboardCorners(gray, (7,5), None)
- 
+    ret, corners = cv.findChessboardCorners(gray, (8,6), None)
+    print(ret)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
@@ -33,24 +33,31 @@ for fname in images:
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
  
+        img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        plt.imshow(img_rgb)
+        plt.axis('off')
+        plt.show()
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,5), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        #cv.drawChessboardCorners(img, (8,6), corners2, ret)
+        #cv.imshow('img', img)
+        #cv.waitKey(500)
  
-cv.destroyAllWindows()
+#cv.destroyAllWindows()
 
-"""  
-# cchequear posicion de linea
+
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
+print('mtx: \n')
+print(mtx)
 # refino la matriz
 # left12 es una imagen nueva, no del set que paso
-img = cv.imread('left12.jpg')
+img = cv.imread('IMG_4204.png')
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-# --------- 2 formas de undistorsionar ------------
+print('newcameramtx: \n')
+print(newcameramtx)
+
+# --------- 2 formas de undistorsionar ------------ probar cual da un mejor resultado
 
 # 1)
 # undistort
@@ -60,7 +67,10 @@ dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
 cv.imwrite('calibresult.png', dst)
+print('dist: \n')
+print(dst)
 
+""""
 # 2)
 # undistort
 mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
@@ -70,11 +80,5 @@ dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
 cv.imwrite('calibresult.png', dst) #guardo la img con el roi de interes
-
-# falta guardar la matriz, 'newcameramtx' ?
-
-# es necesario calcular el error? shall we?
-
-cv.destroyAllWindows()
 
 """
